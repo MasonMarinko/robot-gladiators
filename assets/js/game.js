78cf42e88b509fd10e82c201ba43d2513a861c0b
MasonM <Mason.P.Marinko@gmail.com>
2020-05-12 17:39:59 -0600

    Updated game to run a full round

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy robots
+//      * Fight all enemy robots
+//      * Defeat each enemy robot
+// "LOSE" - Player robot's health is zero of less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -6,17 +12,15 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert users that they are starting the round    
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-    // Subtract the value of 'playerAttack' from the value of 'enemyHealth and use that result to update the value in the 'enemyHealth' variable.
-    enemyHealth = enemyHealth - playerAttack;
     // if player chooses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
         // remove enemies health by subtracting the amount set in the playerAttack variable
@@ -69,4 +73,6 @@ else {
 
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
