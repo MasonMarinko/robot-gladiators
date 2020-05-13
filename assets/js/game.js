83b7130c7a3da25bea1be2045607feebd4fc86d2
MasonM <Mason.P.Marinko@gmail.com>
2020-05-12 18:01:35 -0600

    updated code with a while loop to loop through until someone has died. Also updated so you now fight all 3 robots, not just 1 and done.

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,8 +17,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert users that they are starting the round    
-    window.alert("Welcome to Robot Gladiators!");
+    // repeat and execute as long as the enemy robot is alive
+    while(enemyHealth > 0) {
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
     // if player chooses to fight, then fight
@@ -71,8 +71,13 @@ else {
     window.alert("You need to pick a valid option. Try again!");
 }
 
+    }
 };
 
+
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    // call fight function with enemy robot
+    fight(pickedEnemyName);
 }
