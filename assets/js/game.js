c3345a73acfc7d6029a41c4b78f95809becbe821
MasonM <Mason.P.Marinko@gmail.com>
2020-05-13 14:58:10 -0600

    Updating current code, issue currently with else window alert at bottom of code

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,11 +71,27 @@ var fight = function(enemyName) {
       }
     }
   };
+// function to start a new game
 
+    for (var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            // let user know what round they are in, remember that arrays start at 0 so we need to add 1
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
 
-for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    // call fight function with enemy robot
-    fight(pickedEnemyName);
-}
+            // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+
+        // use debugger to pause script from running and check what's going on at that point in code
+        // debugger;
+
+        // pass the pickedEnemyNane variable's value into the fight function, where it will assume the value of the enemyName parameters
+        fight(pickedEnemyName);
+        } 
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+        break;
+       }
+    }
